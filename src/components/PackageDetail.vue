0ea91e3f5bd73add24b02e39e794caea3addd274
suleymantaspinar <sl.taspinar@gmail.com>
2020-08-25 00:17:07 +0300

    Package-Detail added

---
+++ src/components/PackageDetail.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <table class="table table-striped package-meta">
+            <tbody>
+                <tr v-for="(item,key) in packageInfo">
+                    <th> {{ key }} </th>
+                    <td v-html="item"> 
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <h4 class="pl-1">Methods</h4>
+            <table v-if="packageMethods.length == 0">
+                <tr>
+                    <td>
+                        No methods
+                    </td>
+                </tr>
+            </table>
+
+            <table class="table table-hover package-table" v-else>
+                <thead>
+                    <th style="width: 100px">
+                        Name
+                    </th>
+                    <th>
+                        Description
+                    </th>
+                </thead>
+                <tbody>
+                    <tr
+                    v-for="methodItem in packageMethods"
+                    @click="showMethod(methodItem.name)"
+                    :title="methodItem.name"
+                    >
+                    <td class="text-primary package-name">
+                        {{ (methodItem.name) }}
+                    </td>
+                    <td
+                        :title="methodItem.description"
+                        class="nowrap"
+                        style="max-width: 1px"
+                        v-html="(methodItem.description)"
+                    ></td>
+                    </tr>
+                </tbody>
+            </table>
+
+    </div>  
+</template>
+
+<script>
+export default {
+    props:	["packageInfo","packageMethods"],
+
+}
+</script>
+
+<style>
+
+</style>
